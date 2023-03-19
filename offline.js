9ac3b2c54756cf99ea3e589f74110f6bbb26217a
Nasyath Faykar <77221570+faykarr@users.noreply.github.com>
2023-03-20 00:21:07 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1679245854,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/ground-sheet0.png",
+		"images/bat-sheet0.png",
+		"images/bat-sheet1.png",
+		"images/bat-sheet2.png",
+		"images/scoretext.png",
+		"images/spring-sheet0.png",
+		"images/spring-sheet1.png",
+		"images/gameoverbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
